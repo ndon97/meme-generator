fb16b2b3fc23fc3db085a8a32d0f1c88c8cc93b7
Nick Don <nick.don.1102@gmail.com>
2022-09-18 16:14:43 +1000

    addding rendering of images

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -2,11 +2,21 @@ import React from "react";
 import memesData from "../memesData.js";
 
 export default function Meme() {
+  const [meme, setMeme] = React.useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg",
+  });
+  const [allMemeImages, setAllMemeImages] = React.useState(memesData);
+
   function getMemeImage() {
-    const memesArray = memesData.data.memes;
+    const memesArray = allMemeImages.data.memes;
     const randomNumber = Math.floor(Math.random() * memesArray.length);
     const url = memesArray[randomNumber].url;
-    console.log(url);
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      randomImage: url,
+    }));
   }
 
   return (
@@ -18,6 +28,7 @@ export default function Meme() {
           Get a new meme image 🖼
         </button>
       </div>
+      <img className="meme--imgae" src={meme.randomImage} alt="" />
     </main>
   );
 }
